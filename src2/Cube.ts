7660a4a9f4eacb081ba259554726437d55909cb9
Nam Nguyen <34676202+soukipedia@users.noreply.github.com>
2025-05-17 11:27:58 +0700

    update

--- src2/Cube.ts
+++ src2/Cube.ts
@@ -1,65 +1,58 @@
 export type CubeName =
-	| 'Roccia'
-	| 'Brant'
-	| 'Cantarella'
-	| 'Zani'
-	| 'Cartethyia'
-	| 'Phoebe';
+  | "Roccia"
+  | "Brant"
+  | "Cantarella"
+  | "Zani"
+  | "Cartethyia"
+  | "Phoebe";
 
 export type GameData = {
-	[cube in CubeName]?: {
-		[key: string]: number;
-	};
+  [cube in CubeName]: {
+    [key: string]: number;
+    extraSteps: number;
+  };
 };
 
 export interface Pad {
-	stack: Cube[];
-	position: number;
+  stack: Cube[];
+  position: number;
 }
 
 interface CubeMethod {
-	(cubes: Cube[], pads: Pad[], gameData: GameData): void;
+  (cubes: Cube[], pads: Pad[], gameData: GameData): void;
 }
 
 interface CubeConfig {
-	name: CubeName;
-	gameStart?: CubeMethod;
-	roundStart?: CubeMethod;
-	getExtraStepsBeforeMove?: (
-		cubes: Cube[],
-		pads: Pad[],
-		gameData: GameData
-	) => number;
-	roundEnd?: CubeMethod;
-	onPassedBy?: (
-		passedByStack: Cube[],
-		currentPad: Pad,
-		gameData: GameData
-	) => void;
-	afterMove?: CubeMethod;
-	roll?: () => number;
+  name: CubeName;
+  gameStart?: CubeMethod;
+  roundStart?: CubeMethod;
+  getExtraStepsBeforeMove?: (
+    cubes: Cube[],
+    pads: Pad[],
+    gameData: GameData,
+  ) => number;
+  roundEnd?: CubeMethod;
+  onPassedBy?: (targetPad: Pad, currentPad: Pad, gameData: GameData) => void;
+  afterMove?: CubeMethod;
+  roll?: () => number;
 }
 
 export default class Cube implements CubeConfig {
-	name: CubeName;
-	gameStart?: CubeMethod;
-	roundStart?: CubeMethod;
-	getExtraStepsBeforeMove?: (
-		cubes: Cube[],
-		pads: Pad[],
-		gameData: GameData
-	) => number;
-	roundEnd?: CubeMethod;
-	onPassedBy?: (
-		passedByStack: Cube[],
-		currentPad: Pad,
-		gameData: GameData
-	) => void;
-	afterMove?: CubeMethod;
-	roll?: () => number;
+  name: CubeName;
+  gameStart?: CubeMethod;
+  roundStart?: CubeMethod;
+  getExtraStepsBeforeMove?: (
+    cubes: Cube[],
+    pads: Pad[],
+    gameData: GameData,
+  ) => number;
+  roundEnd?: CubeMethod;
+  onPassedBy?: (targetPad: Pad, currentPad: Pad, gameData: GameData) => void;
+  afterMove?: CubeMethod;
+  roll?: () => number;
 
-	constructor(config: CubeConfig) {
-		this.name = config.name;
-		Object.assign(this, config);
-	}
+  constructor(config: CubeConfig) {
+    this.name = config.name;
+    Object.assign(this, config);
+  }
 }
--- src2/Index.ts
+++ src2/Index.ts
@@ -1,147 +1,145 @@
-import Cube, { CubeName, Pad } from './Cube';
-import Game from './Game';
+import Cube, { CubeName, GameData, Pad } from "./Cube";
+import Game from "./Game";
 
 function chance(probability: number) {
-	return Math.random() < probability;
+  return Math.random() < probability;
 }
 
 function getAllCubeScores(pads: Pad[]): { [cube in CubeName]: number } {
-	const result = {
-		Roccia: 0,
-		Brant: 0,
-		Cantarella: 0,
-		Zani: 0,
-		Cartethyia: 0,
-		Phoebe: 0,
-	};
-	pads.forEach((pad) => {
-		pad.stack.forEach((cubeInStack, indexInStack) => {
-			result[cubeInStack.name] = pad.position + indexInStack * 0.1;
-		});
-	});
-	return result;
+  const result = {
+    Roccia: 0,
+    Brant: 0,
+    Cantarella: 0,
+    Zani: 0,
+    Cartethyia: 0,
+    Phoebe: 0,
+  };
+  pads.forEach((pad) => {
+    pad.stack.forEach((cubeInStack, indexInStack) => {
+      result[cubeInStack.name] = pad.position + indexInStack * 0.1;
+    });
+  });
+  return result;
 }
 
 const Roccia: Cube = {
-	name: 'Roccia',
-	getExtraStepsBeforeMove(cubes, pads, gameData) {
-		const lastCube = cubes.at(-1);
-		if (lastCube?.name === 'Roccia') {
-			console.log(`!!! Roccia's ability: grants 2 extra steps`);
-			return 2;
-		}
-		return 0;
-	},
+  name: "Roccia",
+  getExtraStepsBeforeMove(cubes, pads, gameData) {
+    const lastCube = cubes.at(-1);
+    if (lastCube?.name === this.name) {
+      console.log(`!!! Roccia's ability: grants 2 extra steps`);
+      gameData[this.name].extraSteps = 2;
+      return 2;
+    }
+    return 0;
+  },
 };
 
 const Brant: Cube = {
-	name: 'Brant',
-	getExtraStepsBeforeMove(cubes, pads, gameData) {
-		const firstCube = cubes.at(0);
-		if (firstCube?.name === 'Brant') {
-			console.log(`!!! Brant's ability: grants 2 extra steps`);
-			return 2;
-		}
-		return 0;
-	},
+  name: "Brant",
+  getExtraStepsBeforeMove(cubes, pads, gameData) {
+    const firstCube = cubes.at(0);
+    if (firstCube?.name === this.name) {
+      console.log(`!!! Brant's ability: grants 2 extra steps`);
+      gameData[this.name].extraSteps = 2;
+      return 2;
+    }
+    return 0;
+  },
 };
 
 const Cantarella: Cube = {
-	name: 'Cantarella',
-	onPassedBy(passedByStack, currentPad, gameData) {
-		if (gameData.Cantarella?.isAbilityTrigged) return;
-		const cubeIndex = currentPad.stack.findIndex(
-			(cube) => cube.name === 'Cantarella'
-		);
+  name: "Cantarella",
+  onPassedBy(targetPad, currentPad, gameData) {
+    if (gameData.Cantarella.abilityTriggered) return;
+    const cubeIndex = currentPad.stack.findIndex(
+      (cube) => cube.name === this.name,
+    );
 
-		if (cubeIndex < 0) throw "Can't find Cantarella in stack!";
-		const [cantaCube] = currentPad.stack.splice(cubeIndex, 1);
-		passedByStack.push(cantaCube);
+    if (cubeIndex < 0) throw "Can't find Cantarella in stack!";
+    const [cantaCube] = currentPad.stack.splice(cubeIndex, 1);
+    targetPad.stack.unshift(cantaCube);
 
-		if (!gameData.Cantarella) {
-			gameData.Cantarella = {};
-		}
+    console.log(
+      `### Cantarella's ability: move together with ${
+        targetPad.stack.at(1)?.name
+      } [${targetPad.stack.map((c) => c.name).join(", ")}]`,
+    );
 
-		console.log(
-			`### Cantarella's ability: move together with ${
-				passedByStack.at(0)?.name
-			} [${passedByStack.map((c) => c.name).join(', ')}]`
-		);
-
-		gameData.Cantarella.isAbilityTrigged = 1;
-	},
+    gameData[this.name].extraSteps += targetPad.position - currentPad.position;
+    gameData[this.name].abilityTriggered = 1;
+  },
 };
 
 const Zani: Cube = {
-	name: 'Zani',
-	roll: () => (chance(0.5) ? 1 : 3),
-	getExtraStepsBeforeMove(cubes, pads, gameData) {
-		if (gameData.Zani?.nextTurnExtraSteps) {
-			const { nextTurnExtraSteps } = gameData.Zani;
-			gameData.Zani.nextTurnExtraSteps = 0;
-			console.log(
-				`!!! Zani's ability: grants ${nextTurnExtraSteps} extra steps`
-			);
-			return nextTurnExtraSteps;
-		}
-		return 0;
-	},
-	roundEnd(cubes, pads, gameData) {
-		const zaniPad = pads.find((pad) => pad.stack.includes(this));
-		if (!zaniPad) throw "Can't find Zani's current pad";
-		if (zaniPad.stack.at(-1) !== this) {
-			if (chance(0.4)) {
-				console.log(
-					`!!! Zani's ability: grants 2 extra steps in the next turn`
-				);
-				if (!gameData.Zani) {
-					gameData.Zani = {};
-				}
-				gameData.Zani.nextTurnExtraSteps = 2;
-			}
-		}
-	},
+  name: "Zani",
+  roll: () => (chance(0.5) ? 1 : 3),
+  getExtraStepsBeforeMove(cubes, pads, gameData) {
+    if (gameData[this.name].nextTurnExtraSteps) {
+      const { nextTurnExtraSteps } = gameData.Zani;
+      gameData[this.name].nextTurnExtraSteps = 0;
+      console.log(
+        `!!! Zani's ability: grants ${nextTurnExtraSteps} extra steps`,
+      );
+      gameData[this.name].extraSteps += nextTurnExtraSteps;
+      return nextTurnExtraSteps;
+    }
+    return 0;
+  },
+  roundEnd(cubes, pads, gameData) {
+    const zaniPad = pads.find((pad) => pad.stack.includes(this));
+    if (!zaniPad) throw "Can't find Zani's current pad";
+    if (zaniPad.stack.at(-1) !== this) {
+      if (chance(0.4)) {
+        console.log(
+          `!!! Zani's ability: grants 2 extra steps in the next turn`,
+        );
+        gameData[this.name].nextTurnExtraSteps = 2;
+        gameData[this.name].abilityTriggered =
+          (gameData[this.name].abilityTriggered ?? 0) + 1;
+      }
+    }
+  },
 };
 
 const Cartethyia: Cube = {
-	name: 'Cartethyia',
-	getExtraStepsBeforeMove(cubes, pads, gameData) {
-		if (!gameData.Cartethyia?.isAbilityTrigged) return 0;
-		// if (chance(0.6)) {
-		console.log(`!!! Cartethyia's ability: grants 2 extra steps`);
-		return 2;
-		// }
-		// return 0;
-	},
-	afterMove(cubes, pads, gameData) {
-		if (gameData.Cartethyia?.isAbilityTrigged) return;
+  name: "Cartethyia",
+  getExtraStepsBeforeMove(cubes, pads, gameData) {
+    if (!gameData[this.name].abilityTriggered) return 0;
+    console.log(`!!! Cartethyia's ability: grants 2 extra steps`);
+    gameData[this.name].extraSteps += 2;
+    return 2;
+  },
+  afterMove(cubes, pads, gameData) {
+    if (gameData[this.name].abilityTriggered) return;
 
-		const scores = getAllCubeScores(pads);
-		if (
-			chance(0.6) &&
-			Object.keys(scores).every(
-				(name) =>
-					scores[name as CubeName] === 0 ||
-					scores[name as CubeName] >= scores.Cartethyia
-			)
-		) {
-			console.log(
-				`### Cartethyia's ability triggered granting 2 extra steps for remaining turns`
-			);
-			gameData.Cartethyia = { isAbilityTrigged: 1 };
-		}
-	},
+    const scores = getAllCubeScores(pads);
+    if (
+      chance(0.6) &&
+      Object.keys(scores).every(
+        (name) =>
+          scores[name as CubeName] === 0 ||
+          scores[name as CubeName] >= scores[this.name],
+      )
+    ) {
+      console.log(
+        `### Cartethyia's ability triggered granting 2 extra steps for remaining turns`,
+      );
+      gameData[this.name].abilityTriggered = 1;
+    }
+  },
 };
 
 const Phoebe: Cube = {
-	name: 'Phoebe',
-	getExtraStepsBeforeMove(cubes, pads, gameData) {
-		if (chance(0.5)) {
-			console.log(`!!! Phoebe's ability: grants 1 extra step`);
-			return 1;
-		}
-		return 0;
-	},
+  name: "Phoebe",
+  getExtraStepsBeforeMove(cubes, pads, gameData) {
+    if (chance(0.5)) {
+      console.log(`!!! Phoebe's ability: grants 1 extra step`);
+      gameData[this.name].extraSteps += 1;
+      return 1;
+    }
+    return 0;
+  },
 };
 
 // const game = new Game({
@@ -152,66 +150,125 @@ const Phoebe: Cube = {
 // game.start();
 
 function sim(
-	cubes: Cube[],
-	n: number,
-	top: number,
-	isLogging: boolean = false
+  cubes: Cube[],
+  n: number,
+  top: number,
+  pads?: Pad[],
+  isLogging: boolean = false,
 ) {
-	const log = console.log;
-	if (!isLogging) {
-		console.log = () => {};
-	}
+  const log = console.log;
+  if (!isLogging) {
+    console.log = () => {};
+  }
 
-	const cubeData: { [cube in CubeName]: number } = {
-		Roccia: 0,
-		Brant: 0,
-		Cantarella: 0,
-		Zani: 0,
-		Cartethyia: 0,
-		Phoebe: 0,
-	};
+  const cubeData: { [cube in CubeName]: number } = {
+    Roccia: 0,
+    Brant: 0,
+    Cantarella: 0,
+    Zani: 0,
+    Cartethyia: 0,
+    Phoebe: 0,
+  };
 
-	let carte = 0;
+  const analytics: GameData = {
+    Roccia: { extraSteps: 0 },
+    Brant: { extraSteps: 0 },
+    Cantarella: { extraSteps: 0, abilityTriggered: 0 },
+    Zani: { extraSteps: 0 },
+    Cartethyia: { extraSteps: 0, abilityTriggered: 0 },
+    Phoebe: { extraSteps: 0 },
+  };
 
-	let i = 0;
-	while (i < n) {
-		i += 1;
-		const game = new Game({
-			cubes,
-			length: 24,
-		});
-		game.start();
-		const scores = getAllCubeScores(game.pads);
-		const names = Object.keys(scores) as CubeName[];
-		names.sort((a, b) => scores[b] - scores[a]);
+  let i = 0;
+  while (i < n) {
+    i += 1;
+    const game = new Game({
+      cubes,
+      length: 24,
+      startPads: pads,
+    });
+    game.start();
+    const scores = getAllCubeScores(game.pads);
+    const names = Object.keys(scores) as CubeName[];
+    names.sort((a, b) => scores[b] - scores[a]);
 
-		names.forEach((name, index) => {
-			if (index <= top - 1) {
-				cubeData[name] += 1;
-			}
-		});
+    names.forEach((name, index) => {
+      if (index <= top - 1) {
+        cubeData[name] += 1;
+      }
+    });
 
-		// log(
-		// 	`>>> Score:\n${names
-		// 		.map((name) => `\t${name}: ${scores[name]}`)
-		// 		.join('\n')}`
-		// );
-		if (game.data.Cartethyia?.isAbilityTrigged) {
-			carte += 1;
-		}
-	}
+    // log(
+    // 	`>>> Score:\n${names
+    // 		.map((name) => `\t${name}: ${scores[name]}`)
+    // 		.join('\n')}`
+    // );
 
-	Object.keys(cubeData).forEach((name) => {
-		log(`${name}: ${(100 * cubeData[name as CubeName]) / n}%`);
-	});
-	log(`Cartethyia's ability triggered ${carte} times in ${n} simulations`);
+    cubes.forEach((cube) => {
+      const data = analytics[cube.name];
 
-	console.log = log;
+      data.extraSteps += game.data[cube.name].extraSteps;
+      data.score = (data.score ?? 0) + scores[cube.name];
+
+      data.extraStepsPerRun =
+        (data.extraStepsPerRun ?? 0) +
+        game.data[cube.name].extraSteps * (1 / n);
+
+      data.extraStepsPerScore = data.extraSteps / data.score;
+
+      data.abilityTriggered =
+        (data.abilityTriggered ?? 0) +
+        (game.data[cube.name].abilityTriggered ?? 0);
+    });
+  }
+
+  log(analytics);
+  log(`In ${n} simulations`);
+  cubes.forEach((cube) => {
+    if (analytics[cube.name].abilityTriggered) {
+      log(
+        `\t${cube.name}'s ability triggered ${analytics[cube.name].abilityTriggered} times`,
+      );
+    }
+    if (analytics[cube.name].extraSteps) {
+      log(
+        `\t${cube.name}'s ability grants ${analytics[cube.name].extraStepsPerRun} extra steps per match, (${analytics[cube.name].extraStepsPerScore * 100}%)`,
+      );
+    }
+  });
+
+  log(`Top ${top} rate:`);
+  Object.keys(cubeData).forEach((name) => {
+    log(`\t${name}: ${(100 * cubeData[name as CubeName]) / n}%`);
+  });
+
+  console.log = log;
 }
 
 // const cubes = [Roccia, Brant, Cantarella, Zani, Cartethyia, Phoebe];
 const cubes = [Roccia, Brant, Cantarella, Zani, Cartethyia, Phoebe];
 
+const seaPads: Pad[] = [
+  {
+    position: 0,
+    stack: [Brant],
+  },
+  {
+    position: -1,
+    stack: [Phoebe, Zani],
+  },
+  {
+    position: -2,
+    stack: [Roccia, Cartethyia],
+  },
+  {
+    position: -3,
+    stack: [Cantarella],
+  },
+];
+
 // sim(100, 4, true);
-sim(cubes, 1000000, 4);
+sim(cubes, 1000000, 1, seaPads);
+// sim(cubes, 1, 1, true);
+
 // sim(1000, 1, true);
